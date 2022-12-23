e9af2de8c9fc37f9e2583c880b27c3e53ce32806
NicolaeMunt <114097090+NicolaeMunt@users.noreply.github.com>
2022-12-23 20:31:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>First php file</title>
+</head>
+<body>
+    <?php 
+   /*$nume="Nicolae";
+   $prenume="Munteanu";
+   echo "My name is $nume $prenume";
+
+   $x=5;
+   var_dump($x);
+
+   echo strpos("Buna dimineata jopa gnilaia tu ce faci","jopa");*/
+
+   echo str_replace("Limba","jopa","Limba noastră-i o comoară
+   În adâncuri înfundată
+   Un şirag de piatră rară
+   Pe moşie revărsată.
+   
+   Limba noastră-i foc ce arde
+   Într-un neam, ce fără veste
+   S-a trezit din somn de moarte
+   Ca viteazul din poveste.
+   
+   Limba noastră-i numai cântec,
+   Doina dorurilor noastre,
+   Roi de fulgere, ce spintec
+   Nouri negri, zări albastre.
+   
+   
+   Limba noastră-i graiul pâinii,
+   Când de vânt se mişcă vara;
+   In rostirea ei bătrânii
+   Cu sudori sfinţit-au ţara.
+   
+   Limba noastră-i frunză verde,
+   Zbuciumul din codrii veşnici,
+   Nistrul lin, ce-n valuri pierde
+   Ai luceferilor sfeşnici.
+   
+   Nu veţi plânge-atunci amarnic,
+   Că vi-i limba prea săracă,
+   Şi-ţi vedea, cât îi de darnic
+   Graiul ţării noastre dragă.
+   
+   Limba noastră-i vechi izvoade.
+   Povestiri din alte vremuri;
+   Şi citindu-le ‘nşirate, –
+   Te-nfiori adânc şi tremuri.
+   
+   Limba noastră îi aleasă
+   Să ridice slava-n ceruri,
+   Să ne spiue-n hram şi-acasă
+   Veşnicele adevăruri.")
+
+
+     ?>
+    
+</body>
+</html>
